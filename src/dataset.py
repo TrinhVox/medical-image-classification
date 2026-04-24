3ff04afafe63eaa9d73de5466e559f3853175c4e
Trinh Vo <trinhvo00@gmail.com>
2026-04-24 01:22:55 +0000

    feat: Added Pytorch dataset wrapper and transformations for training and validation

---
+++ src/dataset.py
@@ -0,0 +1,30 @@
+"""
+Wrap HuggingDace dataset in PyTorch dataset class
+Transform label list into binary vector
+"""
+
+
+import torch
+from torch.utils.data import Dataset
+
+
+
+class ChestXrayDataset(Dataset):
+    def __init__(self, hf_dataset, transform=None):
+        self.data = hf_dataset
+        self.transform = transform
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self,idx):
+        raw_labels = self.data[idx]["labels"]
+        adjusted = [l - 1 for l in raw_labels if l != 0]
+        label = torch.zeros(14, dtype=torch.float)
+        if adjusted:
+            label.scatter_(0, torch.tensor(adjusted), 1)
+        image = self.data[idx]["image"]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+        
+
+        
